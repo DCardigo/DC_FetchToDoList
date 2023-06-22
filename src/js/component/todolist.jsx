311d2f38acde2e525caa073b35c7f31aa0a4ab16
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-06-22 15:34:16 +0000

    css

---
+++ src/js/component/todolist.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+
+const Todo = () => {
+
+    return (
+
+        <div className="justify-content-center">
+
+            <h1 className="text-center mt-5">ToDoList</h1>
+            <ul className="list-group d-flex col-3">
+                <li className="list-group-item">An item</li>
+                <li className="list-group-item">A second item</li>
+                <li className="list-group-item">A third item</li>
+                <li className="list-group-item">A fourth item</li>
+                <li className="list-group-item">And a fifth one</li>
+            </ul>
+        
+        </div>
+    )
+}
+
+
+export default Todo
+

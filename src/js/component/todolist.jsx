a94857463aa99eb564d32944ce12e19307fb4761
DCardigo <123099651+DCardigo@users.noreply.github.com>
2023-06-23 11:15:16 +0000

    Falta el borrado

--- src/js/component/todolist.jsx
+++ src/js/component/todolist.jsx
@@ -1,19 +1,49 @@
-import React from "react";
+import React, {useState} from "react";
 
 
 const Todo = () => {
+
+    // ESTADOS
+
+    const [tarea,setTarea] = useState("");
+    // console.log(tarea)
+
+    const [lista,setLista] = useState([]);
+
+    // console.log(lista);
+
+    // FUNCIÓN KEYDOWN
+
+    const handleKey= (e) => {
+        if (e.key === 'Enter' && tarea != "") {
+            handleLista()
+            setTarea("")
+        }
+
+      };
+
+    //   FUNCION LISTA
+
+    const handleLista = (e) => {
+        setLista([...lista, tarea]);
+     
+      };
+    
+        // console.log(lista);
+
+    const newList = lista.map(function(tarea,i){return <li className="list-group-item" key = {i}>{tarea}</li>})
 
     return (
 
         <div className="justify-content-center">
 
             <h1 className="text-center mt-5">ToDoList</h1>
-            <ul className="list-group d-flex col-3">
-                <li className="list-group-item">An item</li>
-                <li className="list-group-item">A second item</li>
-                <li className="list-group-item">A third item</li>
-                <li className="list-group-item">A fourth item</li>
-                <li className="list-group-item">And a fifth one</li>
+            <ul className="list-group d-flex col-3 shadow-lg mb-5 bg-body rounded">
+                
+                <input type="text" value = {tarea} onChange={(e) => setTarea(e.target.value)} onKeyDown={handleKey}/>
+
+                {newList}
+               
             </ul>
         
         </div>
